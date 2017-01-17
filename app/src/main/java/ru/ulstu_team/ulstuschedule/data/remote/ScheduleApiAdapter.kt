15cc846d96f3c4446fd8f83469232be98711e845
LukinEgor <e.lukin@simcase.ru>
2017-01-17 09:58:43 +0400

    add lesson downloading to api

--- app/src/main/java/ru/ulstu_team/ulstuschedule/data/remote/ScheduleApiAdapter.kt
+++ app/src/main/java/ru/ulstu_team/ulstuschedule/data/remote/ScheduleApiAdapter.kt
@@ -1,20 +1,81 @@
 package ru.ulstu_team.ulstuschedule.data.remote
 
+import com.google.gson.*
 import ru.ulstu_team.ulstuschedule.data.model.Group
+import ru.ulstu_team.ulstuschedule.data.model.Lesson
+import ru.ulstu_team.ulstuschedule.data.model.Teacher
 import java.net.URL
 
 class ScheduleApiAdapter {
+    private val daysOfWeek = listOf("mon", "tue", "wed", "thu", "fri", "sat")
+
     fun getGroups() : List<Group> {
         val request = "http://ec2-52-32-177-83.us-west-2.compute.amazonaws.com:3000/api/v1/groups"
-        val groups = URL(request)
-                .readText()
-                .split(',')
+        val groups = toJsonArray(URL(request).readText())
                 .map { groupName ->
                     val group = Group()
-                    group.name = groupName
+                    group.name = groupName.asString
                     group
                 }
 
         return groups
+    }
+
+    fun getLessons(group : Group) : List<Lesson> {
+        val request =
+                "http://ec2-52-32-177-83.us-west-2.compute.amazonaws.com:3000/api/v1/schedule?group=${group.name}"
+
+        val json = toJsonObject(URL(request).readText())
+
+        return toListLessons(json)
+    }
+
+    private fun toJsonArray(input: String) : JsonArray {
+        return JsonParser().parse(input).asJsonArray
+    }
+
+    private fun toJsonObject(input: String) : JsonObject {
+        return JsonParser().parse(input).asJsonObject
+    }
+
+    private fun toListLessons(json: JsonObject) : List<Lesson> {
+        val numbersOfWeek = listOf(1, 2)
+        return numbersOfWeek
+                .map { week -> jsonWeekToListLessons(json[week.toString()].asJsonObject, week) }
+                .flatten()
+    }
+
+    private fun jsonWeekToListLessons(json: JsonObject, week: Int) : List<Lesson> {
+        return daysOfWeek
+                .map { day -> jsonDayToListLessons(json[day].asJsonObject, week, day) }
+                .flatten()
+    }
+
+    private fun jsonDayToListLessons(json: JsonObject, week: Int, day: String) : List<Lesson> {
+        val numbersOfLesson = listOf(1, 2, 3, 4, 5, 6, 7)
+
+        return numbersOfLesson
+                .filter { number -> isNotEmptyLesson(json[number.toString()]
+                        .asJsonObject) }
+                .map { number -> jsonLessonToObjectLesson(json[number.toString()]
+                        .asJsonObject, week, day, number) }
+    }
+
+    private fun isNotEmptyLesson(lesson : JsonObject) : Boolean {
+        return lesson["subject"].asJsonArray[0].asString != "_"
+    }
+
+    private fun jsonLessonToObjectLesson(json: JsonObject, week: Int, day: String, number: Int) : Lesson {
+        val lesson = Lesson()
+        lesson.name = json["subject"].asJsonArray.joinToString("/").drop(1).dropLast(1)
+        val teacher = Teacher()
+        teacher.name = json["teachers"].asJsonArray.joinToString("/").drop(1).dropLast(1)
+        lesson.teacher = teacher
+        lesson.cabinet = json["classroom"].asJsonArray.joinToString("/").drop(1).dropLast(1)
+        lesson.number = number
+        lesson.numberOfWeek = week
+        lesson.dayOfWeek = daysOfWeek.indexOf(day) + 1
+
+        return lesson
     }
 }
--- app/src/test/java/ru/ulstu_team/ulstuschedule/ScheduleApiAdapterTest.kt
+++ app/src/test/java/ru/ulstu_team/ulstuschedule/ScheduleApiAdapterTest.kt
@@ -5,9 +5,45 @@ import org.junit.Assert.*
 import ru.ulstu_team.ulstuschedule.data.remote.ScheduleApiAdapter
 
 class ScheduleApiAdapterTest{
+    val api = ScheduleApiAdapter()
+
     @Test
-    fun getGroups(){
-        val api = ScheduleApiAdapter()
-        assertEquals(api.getGroups().size, 225)
+    fun getListGroups(){
+        val groups = api.getGroups()
+        assertEquals(groups.size, 225)
+    }
+
+    @Test
+    fun getNameOfGroup(){
+        val groups = api.getGroups()
+        assertEquals(groups.first().name, "ЛМКбд-11")
+    }
+
+    @Test
+    fun getListLessonsOfGroup(){
+        val group = api
+                .getGroups()
+                .filter { group -> group.name == "ПИбд-41"}
+                .first()
+
+        val lessons = api.getLessons(group)
+        assertEquals(lessons.size, 36)
+    }
+
+    @Test
+    fun getLessonInfo(){
+        val group = api
+                .getGroups()
+                .filter { group -> group.name == "ПИбд-41"}
+                .first()
+
+        val lessons = api.getLessons(group)
+        val lesson = lessons
+                .filter { lesson -> lesson.numberOfWeek == 1 && lesson.dayOfWeek == 2 && lesson.number == 4}
+                .first()
+
+        assertEquals("ИИС", lesson.name)
+        assertEquals("КОРУНОВА Н В", lesson.teacher.name)
+        assertEquals("а.411/3", lesson.cabinet)
     }
 }
